e7fae7056c277390cf2f7592843e7dc15640fbcc
mebikrouse <arti0874@gmail.com>
2019-09-27 16:56:44 +0300

    add new support tasks

--- Tasks/Support/LambdaTask.cs
+++ Tasks/Support/LambdaTask.cs
@@ -1,7 +1,39 @@
+using System;
+using HPNS.Tasks.Core;
+using HPNS.Tasks.Core.Exceptions;
+
 namespace HPNS.Tasks.Support
 {
-    public class LambdaTask
+    public class LambdaTask : ITask
     {
+        private Action _lambda;
         
+        public TaskState CurrentState { get; private set; } = TaskState.Waiting;
+        
+        public event EventHandler TaskDidEnd;
+
+        public LambdaTask(Action lambda)
+        {
+            _lambda = lambda;
+        }
+        
+        public void Start()
+        {
+            if (CurrentState != TaskState.Waiting)
+                throw new StartException();
+            
+            _lambda();
+
+            CurrentState = TaskState.Ended;
+            TaskDidEnd?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Abort()
+        {
+            if (CurrentState != TaskState.Running)
+                throw new AbortException();
+
+            CurrentState = TaskState.Aborted;
+        }
     }
 }
--- Tasks/Support/WaitTask.cs
+++ Tasks/Support/WaitTask.cs
@@ -1,7 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using HPNS.Tasks.Core;
+using HPNS.Tasks.Core.Exceptions;
+
 namespace HPNS.Tasks.Support
 {
-    public class WaitTask
+    public class WaitTask : ITask
     {
+        private int _delay;
+        private bool _notifyAboutDelayEnding;
         
+        public TaskState CurrentState { get; private set; } = TaskState.Waiting;
+        
+        public event EventHandler TaskDidEnd;
+
+        public WaitTask(int delay)
+        {
+            _delay = delay;
+        }
+        
+        public void Start()
+        {
+            if (CurrentState != TaskState.Waiting)
+                throw new StartException();
+
+            _notifyAboutDelayEnding = true;
+            _ = WaitForDelay(_delay);
+
+            CurrentState = TaskState.Running;
+        }
+
+        public void Abort()
+        {
+            if (CurrentState != TaskState.Running)
+                throw new AbortException();
+
+            _notifyAboutDelayEnding = false;
+
+            CurrentState = TaskState.Aborted;
+        }
+
+        private async Task WaitForDelay(int delay)
+        {
+            await BaseScript.Delay(delay);
+
+            if (_notifyAboutDelayEnding)
+            {
+                CurrentState = TaskState.Ended;
+                TaskDidEnd?.Invoke(this, EventArgs.Empty);
+            }
+        }
     }
 }
