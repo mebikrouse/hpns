6c6b828395ddc3c0c4605954840c9b76648bf12f
mebikrouse <arti0874@gmail.com>
2019-10-03 21:08:15 +0300

    rebuild ShopRobberyQuestTest

--- QuestTestClient/Tests/ShopRobberyQuestTest.cs
+++ QuestTestClient/Tests/ShopRobberyQuestTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using CitizenFX.Core;
+using HPNS.Core;
 using HPNS.Interactivity.Core;
 using HPNS.Interactivity.Scenarios;
 using HPNS.Interactivity.States;
@@ -24,35 +25,47 @@ namespace QuestTestClient.Tests
             var pedPosition = new Vector3(-46.10585f, -1757.487f, 29.421f);
             var pedHeading = 52.37571f;
             
-            var pedHandle = await CreatePedAtPosition(pedPosition, pedHeading, (uint) GetHashKey("a_m_m_ktown_01"));
+            var pedHandle = await Utility.CreatePedAtPosition(pedPosition, pedHeading, (uint) GetHashKey("a_m_m_ktown_01"));
             SetBlockingOfNonTemporaryEvents(pedHandle, true);
+            SetPedCanRagdollFromPlayerImpact(pedHandle, false);
             PlaceObjectOnGroundProperly(pedHandle);
 
             ShopRobberyScenario shopRobberyScenario;
             Pickup bagPickup = null;
 
-            var tasks = new List<ITask>();
-            tasks.Add(new GoToRadiusAreaTask(new Vector3(-55.11107f, -1759.414f, 28.98155f), 10.0f));
-            tasks.Add(new GoToRadiusAreaTask(new Vector3(-51.2039f, -1754.137f, 29.42102f), 5.0f));
-            tasks.Add(new StateRecoverWaitTask(new AimingAtEntityState(pedHandle)));
-            tasks.Add(shopRobberyScenario = new ShopRobberyScenario(pedHandle));
-            tasks.Add(new LambdaTask(() =>
-            {
-                SetBlockingOfNonTemporaryEvents(pedHandle, false);
-                TaskSmartFleePed(pedHandle, Game.PlayerPed.Handle, 50f, -1, true, true);
-            }));
-            tasks.Add(new LambdaTask(() =>
+            var pedDependedTasks = new List<ITask>();
+            pedDependedTasks.Add(new GoToRadiusAreaTask(new Vector3(-55.11107f, -1759.414f, 28.98155f), 10.0f));
+            pedDependedTasks.Add(new GoToRadiusAreaTask(new Vector3(-51.2039f, -1754.137f, 29.42102f), 5.0f));
+            pedDependedTasks.Add(new StateRecoverWaitTask(new AimingAtEntityState(pedHandle)));
+            pedDependedTasks.Add(shopRobberyScenario = new ShopRobberyScenario(pedHandle));
+            pedDependedTasks.Add(new LambdaTask(() =>
             {
                 bagPickup = World.Current.ObjectManager.AddObject(new Pickup(shopRobberyScenario.BagEntityHandle, 0.5f));
+                
+                SetBlockingOfNonTemporaryEvents(pedHandle, false);
+                SetPedCanRagdollFromPlayerImpact(pedHandle, true);
+                TaskSmartFleePed(pedHandle, Game.PlayerPed.Handle, 50f, -1, true, true);
             }));
-            tasks.Add(new DeferredCreationTask(() => new TakePickupTask(bagPickup)));
-            tasks.Add(new LeaveRadiusAreaTask(new Vector3(-55.11107f, -1759.414f, 28.98155f), 50f));
 
-            var sequentialSetTask = new SequentialSetTask(tasks);
-            sequentialSetTask.TaskDidEnd += CurrentTaskOnTaskDidEnd;
-            sequentialSetTask.Start();
+            var pedDependedRaceParallelTasks = new List<ITask>();
+            pedDependedRaceParallelTasks.Add(new SequentialSetTask(pedDependedTasks));
+            pedDependedRaceParallelTasks.Add(new StateBreakWaitTask(new EntityIsAliveState(pedHandle)));
+            
+            var questTasks = new List<ITask>();
+            questTasks.Add(new RaceParallelSetTask(pedDependedRaceParallelTasks));
+            questTasks.Add(new ConditionalTask(() => World.Current.EntityDeathTracker.IsEntityAlive(pedHandle),
+                new DeferredCreationTask(() =>
+                {
+                    Debug.WriteLine("Cashier is alive. God bless you!!!");
+                    return new TakePickupTask(bagPickup);
+                })));
+            questTasks.Add(new LeaveRadiusAreaTask(new Vector3(-55.11107f, -1759.414f, 28.98155f), 100f));
 
-            _currentTask = sequentialSetTask;
+            var questSequence = new SequentialSetTask(questTasks);
+            questSequence.TaskDidEnd += CurrentTaskOnTaskDidEnd;
+            questSequence.Start();
+
+            _currentTask = questSequence;
         }
 
         protected override void ExecuteAborting()
@@ -65,18 +78,6 @@ namespace QuestTestClient.Tests
         {
             _currentTask.TaskDidEnd -= CurrentTaskOnTaskDidEnd;
             NotifyTaskDidEnd();
-        }
-
-        private static async Task<int> CreatePedAtPosition(Vector3 position, float heading, uint pedHash)
-        {
-            while (!HasModelLoaded(pedHash))
-            {
-                RequestModel(pedHash);
-                await BaseScript.Delay(100);
-            }
-            
-            var ped = CreatePed(0, pedHash, position.X, position.Y, position.Z, heading, true, false);
-            return ped;
         }
     }
 }
