0dbd62866eea400cd594bb77e64686c6b231d5ff
mebikrouse <arti0874@gmail.com>
2019-10-13 03:50:46 +0300

    fix messages and add deletion of previous car

--- CarSpawnerClient/CarSpawner.cs
+++ CarSpawnerClient/CarSpawner.cs
@@ -7,50 +7,74 @@ namespace CarSpawnerClient
 {
     public class CarSpawner : BaseScript
     {
+        private int _prevVehicleHandle;
+        
         public CarSpawner()
         {
             EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
         }
 
-        private static void OnClientResourceStart(string resourceName)
+        private void OnClientResourceStart(string resourceName)
         {
             if (GetCurrentResourceName() != resourceName) return;
             
-            RegisterCommand("car", new Action<int, List<object>, string>(async (source, args, raw) =>
+            RegisterCommand("spawncar", new Action<int, List<object>, string>(async (source, args, raw) =>
             {
-                // account for the argument not being passed
-                var model = "adder";
-                if (args.Count > 0)
+                if (args.Count != 1)
                 {
-                    model = args[0].ToString();
+                    PrintToChat("Usage: /spawncar [model]");
+                    return;
                 }
+
+                var model = args[0].ToString();
             
-                // check if the model actually exists
-                // assumes the directive `using static CitizenFX.Core.Native.API;`
                 var hash = (uint) GetHashKey(model);
                 if (!IsModelInCdimage(hash) || !IsModelAVehicle(hash))
                 {
-                    TriggerEvent("chat:addMessage", new 
-                    {
-                        color = new[] { 255, 0, 0 },
-                        args = new[] { "[CarSpawner]", $"It might have been a good thing that you tried to spawn a {model}. Who even wants their spawning to actually ^*succeed?" }
-                    });
+                    PrintToChat($"Unable to spawn car with model {model}.");
                     return;
                 }
             
-                // create the vehicle
                 var vehicle = await World.CreateVehicle(model, Game.PlayerPed.Position, Game.PlayerPed.Heading);
-                
-                // set the player ped into the vehicle and driver seat
                 Game.PlayerPed.SetIntoVehicle(vehicle, VehicleSeat.Driver);
-            
-                // tell the player
-                TriggerEvent("chat:addMessage", new 
-                {
-                    color = new[] {255, 0, 0},
-                    args = new[] {"[CarSpawner]", $"Woohoo! Enjoy your new ^*{model}!"}
-                });
+
+                SetModelAsNoLongerNeeded(hash);
+                if (_prevVehicleHandle != 0)
+                    SetEntityAsNoLongerNeeded(ref _prevVehicleHandle);
+
+                _prevVehicleHandle = vehicle.Handle;
+
+                PrintToChat($"Vehicle {model} has been spawned.");
             }), false);
+            
+            RegisterCommand("fix", new Action<int, List<object>, string>((source, args, raw) =>
+            {
+                if (args.Count != 0)
+                {
+                    PrintToChat("Usage: /fix");
+                    return;
+                }
+                
+                var playerVehicle = Game.PlayerPed.CurrentVehicle;
+                if (playerVehicle == null)
+                {
+                    PrintToChat("You have to be in a vehicle to fix it.");
+                    return;
+                }
+                
+                playerVehicle.Repair();
+
+                PrintToChat("Your car has been fixed.");
+            }), false);
+        }
+
+        private static void PrintToChat(string message)
+        {
+            TriggerEvent("chat:addMessage", new 
+            {
+                color = new[] {255, 0, 0},
+                args = new[] {"[CarSpawner]", message}
+            });
         }
     }
 }
